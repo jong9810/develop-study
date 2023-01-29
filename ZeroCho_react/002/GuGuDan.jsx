aa11cd2c8cbd4cb0f1e9cdccc7ff42f74b3197b6
jongin <euncun9810@gmail.com>
2023-01-29 15:35:55 +0900

    ZeroCho react lecture

--- ZeroCho_react/002/GuGuDan.jsx
+++ ZeroCho_react/002/GuGuDan.jsx
@@ -1,7 +1,7 @@
 const React = require('react');
 const { Component } = React;
 
-// 1. 클래스 컴포넌트
+/*/ 1. 클래스 컴포넌트
 class GuGuDan extends Component {
   state = {
     value: '',
@@ -11,14 +11,16 @@ class GuGuDan extends Component {
   };
   input;
 
-  onSubmitForm(e) {
+  onSubmitForm = (e) => {
     e.preventDefault();
     if (parseInt(this.state.value) === this.state.first * this.state.second) {
-      this.setState({
-        value: '',
-        result: '정답: ' + this.state.value,
-        first: Math.ceil(Math.random() * 9),
-        second: Math.ceil(Math.random() * 9),
+      this.setState((prevState) => {
+        return {
+          value: '',
+          result: '정답: ' + prevState.value,
+          first: Math.ceil(Math.random() * 9),
+          second: Math.ceil(Math.random() * 9),
+        };
       });
     } else {
       this.setState({
@@ -27,13 +29,13 @@ class GuGuDan extends Component {
       });
     }
     this.input.focus();
-  }
-  onChangeInput(e) {
+  };
+  onChangeInput = (e) => {
     this.setState({ value: e.target.value });
-  }
-  onInputRef(c) {
+  };
+  onInputRef = (c) => {
     this.input = c;
-  }
+  };
 
   render() {
     return (
@@ -50,31 +52,46 @@ class GuGuDan extends Component {
     );
   }
 }
-//
+/*/
 
-/*/ 2. 함수 컴포넌트
+// 2. 함수 컴포넌트
 const GuGuDan = () => {
   const [value, setValue] = React.useState('');
   const [result, setResult] = React.useState('');
   const [first, setFirst] = React.useState(Math.ceil(Math.random() * 9));
   const [second, setSecond] = React.useState(Math.ceil(Math.random() * 9));
+  const inputRef = React.useRef(null);
 
-  const onSubmitForm = () => {};
-  const onChangeInput = () => {};
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (parseInt(value) === first * second) {
+      setValue('');
+      setResult('정답: ' + value);
+      setFirst(Math.ceil(Math.random() * 9));
+      setSecond(Math.ceil(Math.random() * 9));
+    } else {
+      setValue('');
+      setResult('오답');
+    }
+    inputRef.current.focus();
+  };
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
+  };
 
   return (
     <div>
       <div>
         {first}곱하기 {second}은(는)?
       </div>
-      <form>
-        <input type='number' />
+      <form onSubmit={onSubmitForm}>
+        <input type='number' value={value} onChange={onChangeInput} ref={inputRef} />
         <button>입력!</button>
       </form>
       <div id='result'>{result}</div>
     </div>
   );
 };
-/*/
+//
 
 module.exports = GuGuDan;
