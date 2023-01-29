f0a91df07cdabc1b57afff7c8b0df4c71fc7ce25
jongin <euncun9810@gmail.com>
2023-01-29 12:18:49 +0900

    ZeroCho react lecture

--- ZeroCho_react/003/client.jsx
+++ ZeroCho_react/003/client.jsx
@@ -1,4 +1,6 @@
 const React = require('react');
 const ReactDom = require('react-dom');
 
+const WordRelay = require('./WordRelay');
+
 React.render(<WordRelay />, document.querySelector('#root'));
--- ZeroCho_react/003/webpack.config.js
+++ ZeroCho_react/003/webpack.config.js
@@ -1,3 +1,18 @@
+const path = require('path'); // node에서 제공하는 경로를 조작하는 프로그램(파일)?
+
 module.exports = {
-  //
+  name: 'word-relay-setting',
+  mode: 'development', // 실제 서비스에서는 production으로 바꾸면 됨.
+  devtool: 'eval',
+
+  // entry랑 output이 제일 중요하다.
+  entry: {
+    app: ['./client.jsx', 'WordRelay.jsx'],
+  }, // 입력
+  output: {
+    // path.join(기존경로, 합칠경로) -> 기존 경로와 합칠 경로를 받아서 하나의 경로로 함쳐준다.
+    // __dirname : 현재 폴더 경로
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js',
+  }, // 출력
 };
