00925ef1b95e74d11023b164c492d7f51147b65f
jongin <euncun9810@gmail.com>
2023-01-31 14:14:14 +0900

    ZeroCho react lecture

---
+++ 002/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  name: '끝말잇기',
+  mode: 'development',
+
+  entry: {
+    //
+  },
+
+  module: {
+    //
+  },
+
+  output: {
+    //
+  },
+
+  devServer: {
+    //
+  },
+};
