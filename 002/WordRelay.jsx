16d72ed9b565925e4f1ee9396600f1081d01be81
jongin <euncun9810@gmail.com>
2023-01-31 15:46:32 +0900

    ZeroCho react lecture

--- 002/WordRelay.jsx
+++ 002/WordRelay.jsx
@@ -1,9 +1,57 @@
 const React = require('react');
-// const { Component } = React;
-const { useState, useRef } = React;
+const { Component } = React;
+// const { useState, useRef } = React;
 
+/*/ 클래스 컴포넌트
+class WordRelay extends Component {
+  state = {
+    word: '제로초 컴포넌트',
+    value: '',
+    result: '',
+  };
+  input;
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+      this.setState({
+        word: this.state.value,
+        value: '',
+        result: '딩동댕',
+      });
+    } else {
+      this.setState({
+        value: '',
+        result: '땡',
+      });
+    }
+    this.input.focus();
+  };
+  onChange = (e) => {
+    this.setState({ value: e.target.value });
+  };
+  inputRef = (c) => {
+    this.input = c;
+  };
+
+  render() {
+    return (
+      <div>
+        <div>{this.state.word}</div>
+        <form onSubmit={this.onSubmit}>
+          <input type='text' onChange={this.onChange} value={this.state.value} ref={this.inputRef} />
+          <button>입력</button>
+        </form>
+        <div>{this.state.result}</div>
+      </div>
+    );
+  }
+}
+/*/
+
+// 함수 컴포넌트(Hooks)
 const WordRelay = () => {
-  const [word, setWord] = useState('제로초');
+  const [word, setWord] = useState('제로초 함수');
   const [value, setValue] = useState('');
   const [result, setResult] = useState('');
   const inputRef = useRef(null);
@@ -36,5 +84,6 @@ const WordRelay = () => {
     </div>
   );
 };
+//
 
 module.exports = WordRelay;
