a3ccfa153bd2e31d8c78ef354beea9d7ee55749c
jongin <euncun9810@gmail.com>
2023-02-06 21:01:12 +0900

    zerocho react

--- zerocho_react/004/ResponseCheck-function.jsx
+++ zerocho_react/004/ResponseCheck-function.jsx
@@ -1,12 +1,12 @@
-import React, { useState /*, useRef, useMemo, useCallback, useEffect */ } from 'react';
+import React, { useState, useRef /*, useMemo, useCallback, useEffect */ } from 'react';
 
-let startTime;
-let timeout;
 const ResponseCheck = () => {
   const [state, setState] = useState('waiting');
   const [message, setMessage] = useState('클릭해서 시작하세요.');
   const [result, setResult] = useState([]);
-  let endTime;
+  const timeout = useRef(null);
+  const startTime = useRef();
+  const endTime = useRef();
 
   const onReset = () => {
     setResult([]);
@@ -15,9 +15,7 @@ const ResponseCheck = () => {
     return (
       <>
         <div>{result.length === 0 ? null : `평균시간: ${result.reduce((a, c) => a + c) / result.length}ms`}</div>
-        <div>
-          <button onClick={onReset}>리셋</button>
-        </div>
+        <div>{result.length === 0 ? null : <button onClick={onReset}>리셋</button>}</div>
       </>
     );
   };
@@ -25,26 +23,26 @@ const ResponseCheck = () => {
     if (state === 'waiting') {
       setState('ready');
       setMessage('초록색이 되면 클릭하세요.');
-      timeout = setTimeout(() => {
-        startTime = new Date();
+      timeout.current = setTimeout(() => {
+        startTime.current = new Date();
         // console.log(startTime);
         setState('go');
         setMessage('클릭하세요!');
       }, Math.ceil(Math.random() * 2000 + 1500));
       // console.log(timeout);
     } else if (state === 'ready') {
-      clearTimeout(timeout);
-      console.log(timeout);
+      clearTimeout(timeout.current);
+      // console.log(timeout);
       setState('waiting');
       setMessage('너무 성급했습니다..');
     } else if (state === 'go') {
-      endTime = new Date();
+      endTime.current = new Date();
       // console.log(startTime, endTime, endTime - startTime);
       // console.log(timeout);
       setState('waiting');
       setMessage('클릭해서 시작하세요.');
       setResult((prevResult) => {
-        return [...prevResult, endTime - startTime];
+        return [...prevResult, endTime.current - startTime.current];
       });
     }
   };
@@ -54,7 +52,28 @@ const ResponseCheck = () => {
       <div id='screen' className={state} onClick={onClickScreen}>
         {message}
       </div>
-      <div>{renderAverage()}</div>
+      {renderAverage()}
+
+      {/* renderAverage 함수를 return 안에 if문 사용하는 방법으로 구현하기 */}
+      {/* {(() => {
+        if (result.length === 0) {
+          return null;
+        }
+        return (
+          <>
+            <div>평균시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
+            <div>
+              <button
+                onClick={() => {
+                  setResult([]);
+                }}
+              >
+                리셋
+              </button>
+            </div>
+          </>
+        );
+      })()} */}
     </>
   );
 };
