f8a3f45a9171d3564a4e049379e3ec5da3f1bd53
jongin <euncun9810@gmail.com>
2023-02-07 11:40:22 +0900

    nodejs study

--- nodeJs/003_animal.js
+++ nodeJs/003_animal.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
+const app = express(); // express 서버
 const port = 3000;
+
+// cors 안에 조건을 설정할 수 있다(default: 모든 요청을 허용).
+// 아래의 코드 한 줄이 없다면 다른 html에서 요청이 왔을 때, CORS 오류가 뜨게 된다.
+// app.use(cors());
 
 app.get('/sound/:name', (req, res) => {
   const { name } = req.params;
-  console.log(name);
+  // console.log(name);
 
   if (name === 'dog') {
     res.json({ sound: '멍멍' });
