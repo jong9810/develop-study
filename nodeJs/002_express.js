28a6128e1424ae1e195c3e8cfa51796eedc0b91b
jongin <euncun9810@gmail.com>
2023-02-07 11:15:51 +0900

    nodejs study

--- nodeJs/002_express.js
+++ nodeJs/002_express.js
@@ -8,6 +8,21 @@ app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
+app.get('/user/:id', function (req, res) {
+  const q = req.params;
+  console.log(q.id);
+
+  res.json({ id: q.id });
+});
+
+app.get('/user', function (req, res) {
+  const q = req.query;
+  // console.log(q);
+  // console.log(q.name);
+  // console.log(q.age);
+  res.json({ name: q.name, age: q.age });
+});
+
 // port에 대해서 듣는 중이라는 뜻(서버가 켜져있는 상태)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
