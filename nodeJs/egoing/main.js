e077f9a2e6fab0b9ae11bbe817a2d404405bf7cb
jongin <euncun9810@gmail.com>
2023-02-07 18:25:30 +0900

    nodejs study

--- nodeJs/egoing/main.js
+++ nodeJs/egoing/main.js
@@ -5,14 +5,34 @@ const url = require('url');
 const app = http.createServer(function (req, res) {
   let _url = req.url;
   const queryData = url.parse(_url, true).query;
-  console.log(queryData);
+  let title = queryData.id;
   if (_url == '/') {
-    _url = '/index.html';
+    title = 'Welcome';
   }
   if (_url == '/favicon.ico') {
     return res.writeHead(404);
   }
   res.writeHead(200);
-  res.end(queryData);
+  fs.readFile(`data/${queryData.id}`, 'utf-8', (err, data) => {
+    const template = `
+<!doctype html>
+<html>
+<head>
+  <title>WEB1 - ${title}</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="/">WEB</a></h1>
+  <ol>
+    <li><a href="/?id=HTML">HTML</a></li>
+    <li><a href="/?id=CSS">CSS</a></li>
+    <li><a href="/?id=JavaScript">JavaScript</a></li>
+  </ol>
+  <h2>${title}</h2>
+  <p>${data}</p>
+</body>
+</html>`;
+    res.end(template);
+  });
 });
 app.listen(3000);
