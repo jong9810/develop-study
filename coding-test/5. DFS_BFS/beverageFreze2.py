762e2d1859020f15f1c62b2890b81a8fc9db5ed0
jongin <euncun9810@gmail.com>
2023-02-12 20:06:23 +0900

    coding-test

---
+++ coding-test/5. DFS_BFS/beverageFreze2.py
@@ -0,0 +1,19 @@
+"""2023.02.12"""
+# 문제 설명
+#
+
+# 모듈, 라이브러리 import
+import time
+
+# 입력
+
+
+# 프로그램 코드
+start_time = time.time()
+
+# 방법1.
+
+
+# 프로그램 실행 시간 출력
+end_time = time.time()
+print('\ntime:', end_time - start_time)
