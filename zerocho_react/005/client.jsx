955843541f1a7bf6dc3deb6dc5dfc10763667363
jongin <euncun9810@gmail.com>
2023-02-12 21:29:52 +0900

    zerocho react lecture

---
+++ zerocho_react/005/client.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+
+import Game from './.jsx';
+
+ReactDom.createRoot(document.querySelector('#root')).render(<Game />);
---
+++ zerocho_react/005/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = {
+  name: '005',
+  mode: 'development', // development production
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  entry: {
+    app: ['./client'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: 'babel-loader',
+        exclude: path.join(__dirname, 'node_modules'),
+        options: {
+          presets: [
+            ['@babel/preset-env', { targets: { browsers: ['last 2 chrome versions'] } }],
+            '@babel/preset-react',
+          ],
+          plugins: ['react-refresh/babel'],
+        },
+      },
+    ],
+  },
+  plugins: [new ReactRefreshWebpackPlugin()],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js',
+    publicPath: '/dist',
+  },
+  devServer: {
+    devMiddleware: { publicPath: '/dist' },
+    static: { directory: path.resolve(__dirname) },
+    port: 3000,
+    hot: true,
+  },
+};
