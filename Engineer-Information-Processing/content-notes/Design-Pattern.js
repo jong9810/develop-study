52b9076d0bd585295dd12598623bcecb6e3cf40d
jongin <euncun9810@gmail.com>
2023-03-01 15:25:09 +0900

    engineer-information-processing

---
+++ Engineer-Information-Processing/content-notes/Design-Pattern.js
@@ -0,0 +1,33 @@
+// - 디자인 패턴(Design Pattern)
+// 모듈의 세분화된 역할이나 모듈들 간의 인터페이스 구현 방식을 설계할 때 참조할 수 있는 전형적인 해결방식을 말한다.
+// 디자인 패턴을 통해 설계 문제, 해결방법, 해결방법을 언제 적용해야 할지, 그 결과는 무엇인지 등을 알 수 있다.
+// 디자인 패턴은 한 패턴에서 변형을 가하거나 어떠한 요구사항을 반영하면 다른 패턴으로 변형되는 특징이 있다.
+
+// - GoF(Gang of Four) 디자인 패턴
+// 소프트웨어 공학에서 가장 많이 사용되는 디자인 패턴
+// - 생성 패턴 : 객체를 생성하는 것에 대한 패턴
+// 1) 추상 팩토리(Abstract Factory)
+// 2) 빌더(Builder)
+// 3) 팩토리 메서드(Factory Method)
+// 4) 프로토타입(Prototype)
+// 5) 싱글톤(Singleton)
+// - 구조 패턴 : 구조를 통해 확장성을 꾀하는 패턴
+// 1) 어댑터(Adapter)
+// 2) 브리지(Bridge)
+// 3) 컴포지트(Composite)
+// 4) 데코레이터(Decorator)
+// 5) 파사드(Facade)
+// 6) 플라이웨이트(Flyweight)
+// 7) 프록시(Proxy)
+// - 행위 패턴 : 행위의 변경, 수정 등을 위한 패턴
+// 1) 책임 연쇄(Chain of Responsibility)
+// 2) 커맨드(Command)
+// 3) 인터프리터(Interpreter)
+// 4) 이터레이터(Iterator)
+// 5) 중재자(Mediator)
+// 6) 메멘토(Memento)
+// 7) 옵저버(Observer)
+// 8) 상태(State)
+// 9) 전략(Strategy)
+// 10) 템플릿 메서드(Template Method)
+// 11) 방문자(Visitor)
