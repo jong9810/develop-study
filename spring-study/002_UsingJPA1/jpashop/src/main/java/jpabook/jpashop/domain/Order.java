a1dadc2bf708b0ffbcdce30f644bd3f944058101
jongin <euncun9810@gmail.com>
2023-09-18 22:41:39 +0900

    [Spring] KYH JPA1: develop Order, OrderItem Entity

--- spring-study/002_UsingJPA1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ spring-study/002_UsingJPA1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -66,4 +66,48 @@ public class Order {
         delivery.setOrder(this);
     }
 
+    //==생성 메서드==//
+    // 복잡한 연관관계를 가지는 생성은 생성 메서드를 따로 정의해두는 것이 좋다.
+    // 생성하는 로직을 변경해야할 때 createOrder 메서드만 변경하면 된다.
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송 완료된 상품은 취소가 불가능합니다.");
+        }
+
+        // 주문 취소 처리
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        return orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
+    }
+
+
+
 }
