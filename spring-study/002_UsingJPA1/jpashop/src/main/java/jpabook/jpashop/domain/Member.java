fb1256308bbf4eefbb472f400dd173b94d74d108
jongin <euncun9810@gmail.com>
2023-09-26 18:55:08 +0900

    [Spring] KYH JPA2: Member API

--- spring-study/002_UsingJPA1/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ spring-study/002_UsingJPA1/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,12 +19,14 @@ public class Member {
     @Column(name="member_id")
     private Long id;
 
+    //@NotEmpty
     private String name;
 
     @Embedded
     private Address address;
 
     // mappedBy가 적히면 매핑을 하지 않고 인수에 의해 매핑된 거울일 뿐이라는 뜻(읽기 전용)
+    //@JsonIgnore // JSON에 포함하지 않음.
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 }
