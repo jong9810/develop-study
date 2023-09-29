6cd69c50c80c3d472f1698d9e8ecee8253cd39b3
jongin <euncun9810@gmail.com>
2023-09-29 21:59:04 +0900

    [Spring] KYH JPA2: Open Session In View

--- spring-study/002_UsingJPA1/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ spring-study/002_UsingJPA1/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -10,7 +10,7 @@ import jpabook.jpashop.repository.order.query.OrderFlatDto;
 import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryRepository;
-import jpabook.jpashop.service.OrderService;
+import jpabook.jpashop.service.query.OrderQueryService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -56,16 +56,13 @@ public class OrderApiController {
         return result;
     }
 
+    private final OrderQueryService orderQueryService;
+
     // V2와 V3의 코드는 같지만 repository에서 로직만 다르기 때문에 repository 메서드만 고쳐주면 튜닝이 된다(정규화가 안 된 하나의 테이블).
     // 쿼리는 한 번만 날리지만, 중복 데이터가 너무 많고 페이징 처리를 할 수 없다.
     @GetMapping("/api/v3/orders")
-    public List<OrderDto> ordersV3() {
-        List<Order> orders = orderRepository.findAllWithItem();
-        List<OrderDto> result = orders.stream()
-                .map(o -> new OrderDto(o))
-                .collect(toList());
-
-        return result;
+    public List<jpabook.jpashop.service.query.OrderDto> ordersV3() {
+        return orderQueryService.ordersV3();
     }
 
     // V3를 페이징 처리 할 수 있도록 repository 수정(V3보다 성능은 조금 떨어지지만 페이징 처리 가능).
@@ -196,5 +193,40 @@ public class OrderApiController {
     // V5 : V5는 코드가 복잡하다. 여러 주문을 한꺼번에 조회하는 경우에는 V5 방식을 사용해야 한다(1+N문제 X).
     // V6 : V6는 완전히 다른 접근방식이다. 쿼리 한 번으로 최적화되어서 상당히 좋아보이지만, Order(일대 다에서 일)를 기준으로 페이징이 불가능하다.
     //      실무에서는 페이징 처리가 필요한 경우가 많으므로 사용하기 어렵다. 데이터가 많으면 중복 전송이 증가해서 V5와 비교해서 성능 차이도 미비하다.
+
+
+    // OSIV(관례상)
+    // Open Session In View : 하이버네이트
+    // Open EntityManager In View : JPA
+    // spring.jpa.open-in-view : true(기본값)
+
+    // true일 경우
+    // 애플리케이션 실행시 경고(warn) 로그 출력됨.
+    // 기본적으로 DB 트랜잭션을 시작할 때 영속성 컨텍스트가 DB 커넥션을 가져온다.
+    // open-in-view가 true인 경우, 트랜잭션이 끝나도 DB 커넥션을 가지고 있다가
+    //      (api의 경우) api가 유저한테 반환될 때 DB 커넥션을 반환한다.
+    //      (화면의 경우) 뷰템플릿이 렌더링 될 때 DB 커넥션을 반환한다.
+    // 지연 로딩은 영속성 컨텍스트가 살아있어야 하고, 영속성 컨텍스트는 기본적으로 DB 커넥션을 유지한다(장점).
+    // 이 전략은 너무 오랜 시간동안 DB 커넥션 리소스를 사용하기 때문에, 실시간 트래픽이 중요한 애플리케이션에서는 커넥션이 모자랄 수 있다(장애로 이어짐).
+
+    // false일 경우
+    // 트랜잭션이 시작할 때부터 끝날 때까지만 영속성 컨텍스트, DB 커넥션이 유지된다(커넥션 리소스 낭비 X).
+    // 이 경우에는 모든 지연로딩을 트랜잭션 안에서 처리해야 한다(코드 복잡도 상승, Controller/View에서 지연로딩 불가).
+    // 그리고 view template에서 지연로딩이 작동하지 않는다(트랜잭션이 끝나기 전 지연로딩을 강제로 호출해두어야 함).
+
+    // 커맨드와 쿼리 분리(OSIV 끈 상태)
+    // 보통 비즈니스 로직은 특정 엔티티 몇 개를 등록하거나 수정하는 것이기 때문에 성능이 크게 문제가 되지 않는다.
+    // 그런데 복잡한 화면을 출력하기 위한 쿼리는 화면에 맞춰 성능을 최적화하는 것이 중요하다(조회).
+    // 하지만 그 복잡성에 비해 핵심 비즈니스에 큰 영향을 주는 것은 아니다.
+    // 그렇기 때문에 크고 복잡한 애플리케이션을 개발한다면, 이 둘의 관심사가 명확하게 분리하는 선택은 유지보수 관점에서 충분히 의미있다.
+    // 화면에 맞춘 기능(api)들은 자주 바뀌고 라이프사이클이 빠르다.
+    // 핵심 비즈니스 로직은 화면에 비하면 잘 변경되지 않고 라이프사이클도 화면과 다르다.
+    // ex) OrderService(애플리케이션이 클 경우 더 효율적임)
+    //     1) OrderService : 핵심 비즈니스 로직
+    //     2) OrderQueryService : 화면이나 API에 맞춘 서비스(주로 읽기전용 트랜잭션 사용)
+    //     보통 서비스 계층에서 트랜잭션을 유지하기 때문에 두 서비스 모두 트랜잭션을 유지하면서 지연로딩을 사용할 수 있다.
+
+    // OSIV 킬 경우, 유지보수성 ▲/성능 ▼ : ADMIN 처럼 커넥션을 많이 사용하지 않는 곳(admin 배포를 따로 한다면)
+    // OSIV 끌 경우, 유지보수성 ▼/성능 ▲ : 고객 서비스의 실시간 API, 트래픽이 많은 경우
 
 }
