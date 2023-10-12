58c939f2ff9b7e1cccb4d3040c7e44c83f28926f
jongin <euncun9810@gmail.com>
2023-10-12 21:11:14 +0900

    BOJ 10798, 2563(ing)

---
+++ coding-test/codingtest/src/main/java/org/example/BOJ_10798.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class BOJ_10798 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] arr = new String[5];
+        for (int i = 0; i < arr.length; i++) {
+            String line = sc.nextLine();
+            arr[i] = line;
+        }
+
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (i >= arr[j].length()) continue;
+                System.out.print(arr[j].charAt(i));
+            }
+        }
+
+        sc.close();
+    }
+}
