087c3eea89d24f1358d4cbc052f27f4c4b0a4bed
jongin <euncun9810@gmail.com>
2023-10-19 17:34:27 +0900

    [JPA] KYH ORM: Advanced Mapping Example

--- JPA/003_ORMJPA/jpabook/src/main/java/jpabook/jpashop/JpaMain.java
+++ JPA/003_ORMJPA/jpabook/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,8 +1,6 @@
 package jpabook.jpashop;
 
-import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.Book;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -17,9 +15,11 @@ public class JpaMain {
 
         tx.begin();
         try {
-            Order order = new Order();
-            order.addOrderItem(new OrderItem());
-            em.persist(order);
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("김영한");
+
+            em.persist(book);
 
             tx.commit();
         } catch (Exception e){
