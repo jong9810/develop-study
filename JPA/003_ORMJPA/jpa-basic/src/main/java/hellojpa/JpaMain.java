619e24056e183dc21232c622fa8691c5944b5609
jongin <euncun9810@gmail.com>
2023-10-19 23:15:18 +0900

    [JPA] KYH ORM: Proxy

--- JPA/003_ORMJPA/jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ JPA/003_ORMJPA/jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,8 @@
 package hellojpa;
 
 
+import org.hibernate.Hibernate;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -19,23 +21,28 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("user1");
-            member.setCreatedBy("Kim");
-            member.setCreatedDate(LocalDateTime.now());
-
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("hello1");
+            em.persist(member1);
 
             em.flush();
             em.clear();
 
+            Member refMember = em.getReference(Member.class, member1.getId());
+            System.out.println("refMember = " + refMember.getClass()); // 프록시 클래스 이름
+//            refMember.getUsername(); // 강제 초기화
+            Hibernate.initialize(refMember); // 강제 초기화
+            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(refMember)); // 프록시 초기화 여부
+
             tx.commit();
         } catch (Exception e){
             tx.rollback();
+            e.printStackTrace();
         } finally {
             em.close();
         }
         emf.close();
 
     }
+
 }
--- JPA/003_ORMJPA/jpa-basic/src/main/java/hellojpa/Member.java
+++ JPA/003_ORMJPA/jpa-basic/src/main/java/hellojpa/Member.java
@@ -18,13 +18,14 @@ public class Member extends BaseEntity {
     @GeneratedValue
     @Column(name = "MEMBER_ID")
     private Long id;
+
     @Column(name = "username")
     private String username;
 
     // 읽기 전용으로 일대다 양방향을 구현할 수도 있다.
-//    @ManyToOne//(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "team_id", insertable = false, updatable = false)
-//    private Team team;
+    @ManyToOne//(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id", insertable = false, updatable = false)
+    private Team team;
 
     // 연관관계 편의 메서드
 //    public void changeTeam(Team team) {
@@ -32,15 +33,11 @@ public class Member extends BaseEntity {
 //        team.getMembers().add(this);
 //    }
 
-    @OneToOne
-    @JoinColumn(name = "LOCKER_ID")
-    private Locker locker;
-
 //    @ManyToMany
 //    @JoinTable(name = "MEMBER_PRODUCT")
 //    private List<Product> products = new ArrayList<>();
 
-    @OneToMany(mappedBy = "member")
-    private List<MemberProduct> memberProducts = new ArrayList<>();
+//    @OneToMany(mappedBy = "member")
+//    private List<MemberProduct> memberProducts = new ArrayList<>();
 
 }
