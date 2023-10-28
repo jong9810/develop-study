6af5270ea81966d6c74c85ebeb582577ff672bd1
jongin <euncun9810@gmail.com>
2023-10-28 17:30:33 +0900

    [JPA] KYH ORM: JPQL Projection

--- JPA/003_ORMJPA/hellojpa/src/main/java/org/example/jpql/JPQLMain.java
+++ JPA/003_ORMJPA/hellojpa/src/main/java/org/example/jpql/JPQLMain.java
@@ -27,24 +27,54 @@ public class JPQLMain {
             member.setAge(10);
             em.persist(member);
 
-            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
-            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
-            Query query3 = em.createQuery("select m.username, m.age from Member m");
+            em.flush();
+            em.clear();
 
-            // 결과 조회 API
-            List<Member> resultList = query1.getResultList();
-            Member singleResult = query1.getSingleResult();
-
-            // 파라미터 바인딩
-            List<Member> result1 = em.createQuery("select m from Member m where username =: username", Member.class)
-                    .setParameter("username", "member1")
+            // 엔티티 프로젝션을 하면 결과 엔티티를 영속성 컨텍스트에서 관리한다.
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
                     .getResultList();
 
-            // 위치 기반 바인딩은 사용하지 말자.
-            List<Member> result2 = em.createQuery("select m from Member m where username =?1", Member.class)
-                    .setParameter(1, "member1")
+            // 값이 바뀌었기 때문에 영속성 컨텍스트가 엔티티를 관리한다는 것을 알 수 있다.
+            Member findMember = result.get(0);
+            findMember.setAge(20);
+
+            // 조인 값 조회
+            //List<Team> result2 = em.createQuery("select m.team from Member m", Team.class)
+            //        .getResultList();
+
+            // 위 방식 보다는 아래 방식처럼 조인을 명시해주는 것이 좋다(조인이 일어난다는 것을 알 수 있음).
+            List<Team> result3 = em.createQuery("select t from Member m join m.team t", Team.class)
                     .getResultList();
 
+            // 임베디드 타입 프로젝션
+            List<Address> result4 = em.createQuery("select o.address from Order o", Address.class)
+                    .getResultList();
+
+            // 스칼라 타입 프로젝션
+            // 1) Query 타입으로 조회
+            List result5 = em.createQuery("select distinct m.username, m.age from Member m")
+                    .getResultList();
+
+            Object o = result5.get(0);
+            Object[] result6 = (Object[]) o;
+            System.out.println("result6[0] = " + result6[0]); // username
+            System.out.println("result6[1] = " + result6[1]); // age
+
+            // 2) Object[] 타입으로 조회
+            List<Object[]> result7 = em.createQuery("select distinct m.username, m.age from Member m", Object[].class)
+                    .getResultList();
+
+            Object[] result8 = result7.get(0);
+            System.out.println("result8[0] = " + result8[0]); // username
+            System.out.println("result8[1] = " + result8[1]); // age
+
+            // 3) DTO로 조회(new 키워드 사용)
+            List<MemberDTO> result9 = em.createQuery("select distinct new org.example.jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+
+            MemberDTO memberDTO = result9.get(0);
+            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
+            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
 
             tx.commit();
         } catch (Exception e){
---
+++ JPA/003_ORMJPA/hellojpa/src/main/java/org/example/jpql/MemberDTO.java
@@ -0,0 +1,14 @@
+package org.example.jpql;
+
+import lombok.Getter;
+
+@Getter
+public class MemberDTO {
+    private String username;
+    private int age;
+
+    public MemberDTO(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+}
