84851f9fa102b0ba4069763e2face9812e5f3be5
jongin <euncun9810@gmail.com>
2023-10-30 11:00:43 +0900

    BOJ coding problems

---
+++ coding-test/codingtest/src/main/java/org/example/math/BOJ_2501.java
@@ -0,0 +1,11 @@
+package org.example.math;
+
+import java.util.Scanner;
+
+public class BOJ_2501 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+    }
+}
