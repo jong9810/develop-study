19b281e27705fc418f22c7c06a71a3e53f3857ef
jongin <euncun9810@gmail.com>
2023-11-02 21:09:43 +0900

    [Spring Data JPA] KYH : Setting, Applying, Analyzing Common Interface

--- spring-data-jpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ spring-data-jpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,5 +3,11 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
+// @Repository 어노테이션 생략 가능
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsername(String username);
+
 }
