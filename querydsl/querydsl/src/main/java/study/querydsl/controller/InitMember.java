10091760d27b0d7e04195c1c1abb32c66093f74a
jongin <euncun9810@gmail.com>
2023-11-13 15:19:49 +0900

    [Querydsl] KYH : Develop Search API Controller

---
+++ querydsl/querydsl/src/main/java/study/querydsl/controller/InitMember.java
@@ -0,0 +1,44 @@
+package study.querydsl.controller;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
+
+@Profile("local")
+@Component
+@RequiredArgsConstructor
+public class InitMember {
+
+    private final InitMemberService initMemberService;
+
+    // @PostConstruct와 @Transactional을 (스프링 생명주기 관리 때문에)같이 사용하지 못하기 때문에 내부 서비스 클래스를 정의해서 해주었다.
+    @PostConstruct
+    public void init() {
+        initMemberService.init();
+    }
+
+    @Component
+    static class InitMemberService {
+        @PersistenceContext
+        EntityManager em;
+
+        @Transactional
+        public void init() {
+            Team teamA = new Team("teamA");
+            Team teamB = new Team("teamB");
+            em.persist(teamA);
+            em.persist(teamB);
+
+            for (int i = 0; i < 100; i++) {
+                Team selectedTeam = i % 2 == 0 ? teamA : teamB;
+                em.persist(new Member("member" + i, i, selectedTeam));
+            }
+        }
+    }
+}
