c3b0e8c2b614f23e888f99f6aea79025bd4cd38c
jongin <euncun9810@gmail.com>
2024-01-04 16:06:53 +0900

    [SprinCore1] dependency injection : only java

---
+++ spring-study/002_SpringBasic1/core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,35 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+// 설정 파일은 역할과 구현이 잘 드러나는 것이 좋다. 
+// --> MemberRepository와 DiscountPolicy 객체는 역할이므로 메서드로 빼서 어떤 구현을 선택했는지 잘 보이게 리팩토링.
+public class AppConfig {
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
+}
--- spring-study/002_SpringBasic1/core/src/main/java/hello/core/discount/DiscountPolicy.java
+++ spring-study/002_SpringBasic1/core/src/main/java/hello/core/discount/DiscountPolicy.java
@@ -5,7 +5,7 @@ import hello.core.member.Member;
 public interface DiscountPolicy {
 
     /**
-     * @return 할인 대상 금액
+     * @return 할인되는 금액
      */
     int discount(Member member, int price);
 }
