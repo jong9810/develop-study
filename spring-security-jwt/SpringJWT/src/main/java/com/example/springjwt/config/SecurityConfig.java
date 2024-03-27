708b5941de15beff4cfe5e47eb3d4a7b772ca92d
jongin <euncun9810@gmail.com>
2024-03-27 16:09:24 +0900

    [Spring Security JWT] 개발자 유미 : Security Config 클래스 (STATELESS)

---
+++ spring-security-jwt/SpringJWT/src/main/java/com/example/springjwt/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.example.springjwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // csrf disable
+        // 세션 방식에서는 세션이 고정되기 때문에 csrf 공격에 취약하지만,
+        // JWT 방식은 세션을 stateless 상태로 관리하기 때문에 csrf 공격을 방어하지 않아도 됨.
+        http
+                .csrf((auth) -> auth.disable());
+
+        // form 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.disable());
+
+        // http basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        // 경로별 인가 작업
+        // 어떤 경로의 요청이 들어왔는지에 따라 인증을 할지 그냥 허용할지 설정
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login", "/", "/join").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated());
+
+        // 세션 설정
+        // 세션을 stateless 상태로 생성하도록 설정
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
