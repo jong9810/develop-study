6bc594d04c4d327f7d48c53f770fd870570b2633
jongin <euncun9810@gmail.com>
2024-04-02 16:07:44 +0900

    [Spring Security] 개발자 유미 : 프로젝트 생성 및 Security Config 인가 작업

---
+++ spring-security/TestSecurity/src/main/java/com/example/testsecurity/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.testsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // 1. 인가 작업 : 인가 동작 순서는 requestMatchers 상단부터 아래로(순서를 잘 지켜서 작성해야함!!).
+        // permitAll : 모든 사용자가 로그인을 하지 않아도 접근 가능
+        // hasRole : 로그인 한 후에 특정한 role이 있어야 접근 가능
+        // hasAnyRole : 로그인을 한 후에 여러가지 role 중 하나 이상이 있으면 접근 가능
+        // authenticated : 로그인만 하면 접근 가능
+        // denyAll : 모든 사용자가 로그인을 해도 접근 불가
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/login").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
