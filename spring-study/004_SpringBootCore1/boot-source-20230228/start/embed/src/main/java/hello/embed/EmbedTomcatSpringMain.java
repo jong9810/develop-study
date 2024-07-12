c5653793fe0043cccef7591ab874996c47a7242f
jongin <euncun9810@gmail.com>
2024-07-12 14:06:25 +0900

    [Spring Boot Core1] 김영한 : 내장톰캣 3 - 스프링, 내장톰캣 4 - 빌드와 배포 1

---
+++ spring-study/004_SpringBootCore1/boot-source-20230228/start/embed/src/main/java/hello/embed/EmbedTomcatSpringMain.java
@@ -0,0 +1,40 @@
+package hello.embed;
+
+import hello.spring.HelloConfig;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class EmbedTomcatSpringMain {
+
+    public static void main(String[] args) throws LifecycleException {
+        System.out.println("EmbedTomcatSpringMain.main");
+
+        // 톰캣 설정
+        Tomcat tomcat = new Tomcat();
+        Connector connector = new Connector();
+        connector.setPort(8080);
+        tomcat.setConnector(connector);
+
+        // 스프링 컨테이너 생성
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.register(HelloConfig.class);
+
+        // 스프링 MVC의 디스패처 서블릿 생성, 스프링 컨테이너와 연결
+        DispatcherServlet dispatcher = new DispatcherServlet(appContext);
+
+        // 디스패처 서블릿 등록
+        Context context = tomcat.addContext("", "/");
+        tomcat.addServlet("", "dispatcher", dispatcher);
+        context.addServletMappingDecoded("/", "dispatcher");
+
+        tomcat.start();
+    }
+}
+
+// JAR 파일은 내부에 라이브러리 역할을 하는 JAR 파일을 포함할 수 없다. 포함한다고 해도 인식이 안된다.
+//
+
