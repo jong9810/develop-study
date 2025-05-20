ed14ed4299f4bb3aae035fd3377223074445de87
jongin <euncun9810@gmail.com>
2025-05-20 11:22:14 +0900

    [codingtest] BOJ: modify 28278

--- coding-test/codingtest/src/main/java/org/example/stackquedeque/BOJ_28278_2.java
+++ coding-test/codingtest/src/main/java/org/example/stackquedeque/BOJ_28278_2.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class BOJ_28278_2 {
     static StringBuffer sb = new StringBuffer();
