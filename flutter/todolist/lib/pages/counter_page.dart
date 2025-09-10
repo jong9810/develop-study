3990519e51ee64602d2748a5332d2b0ce55f9b57
jongin <euncun9810@gmail.com>
2025-09-10 23:17:48 +0900

    [flutter] basic

---
+++ flutter/todolist/lib/pages/counter_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({Key? key}) : super(key: key);
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+
+  // variable
+  int  _counter = 0;
+
+  // method
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  // UI (user interface)
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // message
+            Text('You pushed the button this many times:'),
+
+            // counter value
+            Text(_counter.toString(), style: TextStyle(fontSize: 40)),
+
+            // button
+            ElevatedButton(onPressed: _incrementCounter, child: Text('Increment!'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
---
+++ flutter/todolist/lib/pages/first_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:todolist/pages/home_page.dart';
+import 'package:todolist/pages/profile_page.dart';
+import 'package:todolist/pages/second_page.dart';
+import 'package:todolist/pages/settings_page.dart';
+
+class FirstPage extends StatefulWidget {
+  FirstPage({Key? key}) : super(key: key);
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  int _selectedIndex = 0;
+
+  void _navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List _pages = [
+    HomePage(),
+    ProfilePage(),
+    SettingsPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("1st page")),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _navigateBottomBar,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
